372697bdcfd5096e7f31b91931ddc7d5d56631fe
Vik.v.zayats@gmail.com <vik.v.zayats@gmail.com>
2020-11-02 17:10:26 +0300

    Create CompositionElement.java

---
+++ Basics of OOP/Task 5/src/by/zvv/javaonline/part05/task05/builder/CompositionElement.java
@@ -0,0 +1,11 @@
+package by.zvv.javaonline.part05.task05.builder;
+
+import java.math.BigDecimal;
+
+public interface CompositionElement {
+	
+	public String getElementName();
+	
+	public BigDecimal getPrice();
+
+}
